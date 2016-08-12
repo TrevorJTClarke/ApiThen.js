0afbc4db9db7a1d434342a9cf685a8ead769a31f
Trevor Clarke <besnoid@gmail.com>
2016-08-12 14:57:33 -0700

    added a method constructor, query params support

--- ApiThen.js
+++ ApiThen.js
@@ -5,9 +5,10 @@
  */
 class ApiThen {
 
-  constructor(config = {}) {
+  constructor(config = {}, methods = []) {
     this.options = config.options || {}
     this.headers = config.headers || {}
+    this.methods = methods
 
     // TODO:
     // - Setup options
@@ -15,7 +16,46 @@ class ApiThen {
 
     // default Config
     this.options.route = ''
+    this.options.query = ''
     this.required = {}
+
+    // Initializing methods
+    this.methodsInit()
+  }
+
+  /**
+   * AUTO GENERATE METHODS
+   * NOTE: HIGHLY experimental, checking if this is a good idea or not
+   */
+
+  // grabs all methods from config and sends to creator
+  methodsInit () {
+    if (!this.methods || this.methods.length <= 0) return;
+    var _this = this
+
+    // SUPER BRITTLE,
+    // TODO: check for args, fn returns, etc
+    this.methods.forEach(function (method) {
+      let stdMethodFn
+      let stdMethodArgs = ['id']
+
+      // auto generate based on the type passed in
+      if (typeof method === 'string') {
+        stdMethodFn = 'this.options.route += (id) ? "/' + method + '/" + id : "/' + method + '"; return this;'
+        _this.createMethod(method, stdMethodArgs, stdMethodFn)
+      } else {
+
+        stdMethodFn = 'this.options.route += (id) ? "/' + method.name + '/" + id : "/' + method.name + '"; return this;'
+        _this.createMethod(method.name, method.args || stdMethodArgs, method.fn || stdMethodFn)
+      }
+    })
+  }
+
+  // generates the method inside the prototype
+  createMethod (name, args, fn) {
+    this.__proto__[name] = new Function(args, fn)
+
+    return this
   }
 
   /**
@@ -35,12 +75,14 @@ class ApiThen {
     this.required[key] = val
   }
   getRoute () {
-    return this.options.baseUrl + this.options.route
+    return this.options.baseUrl + this.options.route + this.options.query
   }
 
   /**
    * METHODS
    * Responsible for configuring the method used in request
+   *
+   * TODO: Create DRY version
    */
   get () {
     this.setHeader('method', 'GET')
@@ -123,15 +165,44 @@ class ApiThen {
   }
 
   /**
+   * ENDPOINT QUERY PARAMS
+   * All query params specified here are formatted and appended to final route
+   *
+   * EXAMPLES:
+   * - .query({ limit: 20, offset: 40 })
+   * - .query({ sort: 'desc', limit: 20, offset: 40, featured: true })
+   *
+   * OUTPUTS
+   * - https://api.somewhere.com/users?limit=20&offset=40
+   * - https://api.somewhere.com/users?sort=desc&limit=20&&offset=40&featured=true
+   */
+  query (params) {
+    let finalParams = ''
+
+    // setup each param into main params string, formatted of course
+    for (var p in params) {
+      finalParams += `${p}=${params[p]}&`
+    }
+
+    // remove last & -- Is there a better way to do this?
+    finalParams = finalParams.slice(0, -1)
+
+    this.options.query += (finalParams && finalParams.length > 0) ? `?${finalParams}` : ''
+
+    return this;
+  }
+
+  /**
+   * TODO: Assess for need, since we can auto-generate the methods, prolly dont need
    * ENDPOINT METHODS
    * All Methods specified here create short methods for endpoint url chaining
    *
    * IDEA: Create an array in options to generate these. options? sure.
    */
-  users (id) {
-    this.options.route += `/users${id ? '/' + id : ''}`
-    return this
-  }
+  // users (id) {
+  //   // this.options.route += `/users${id ? '/' + id : ''}`
+  //   return this
+  // }
 
 }
 
--- useCases.js
+++ useCases.js
@@ -1,19 +1,34 @@
 // Different Tests!
 
-/*eslint no-console: ["error", { allow: ["log"] }] */
-console.log('ApiThen', ApiThen)
-
 // New instance
 var Api = new ApiThen({
-  options: {
-    baseUrl: 'https://api.somewhere.com'
-  }
-})
+    options: {
+      baseUrl: 'https://randomuser.me/api'
+    }
+  },
+  [{
+    name: 'newMethod',
+    args: ['id', 'name'],
+    fn: 'this.options.route += (id && name) ? "/newMethod/" + id + "/" + name : "/newMethod"; return this;'
+  }, 'users', 'events', 'groups', 'products', 'notifications', 'organizations']
+)
 
-console.log('Api.get().users(1234)', Api.get().users(1234))
+var createTest = Api.get().users().getRoute()
+var createTest2 = Api.get().notifications(100000038).getRoute()
+var createTest3 = Api.get().users(23422342).products().getRoute()
+var createTest4 = Api.get().groups(1000000).notifications(100000038).getRoute()
+var createTest5 = Api.get().users(23422342).products().query({ limit: 20, offset: 40 }).getRoute()
+var createTest6 = Api.get().groups(1000000).notifications().query({ sort: 'desc', limit: 20, offset: 40, featured: true }).getRoute()
 
-Api.get().users(1234).then((res) => {
-  console.log('res', res)
-}, (err) => {
-  console.log('err', err)
-})
+console.log('createTest', createTest)
+console.log('createTest2', createTest2)
+console.log('createTest3', createTest3)
+console.log('createTest4', createTest4)
+console.log('createTest5', createTest5)
+console.log('createTest6', createTest6)
+
+// Api.get().users(1234).then((res) => {
+//   console.log('res', res)
+// }, (err) => {
+//   console.log('err', err)
+// })
